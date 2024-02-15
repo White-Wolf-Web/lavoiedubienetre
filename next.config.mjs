b2942a02574048b9d5429674a9fe7cfc7f66f29a
stephane Gamot <116945810+White-Wolf-Web@users.noreply.github.com>
2024-02-15 21:26:54 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {  async redirects() {
-    return [
-      {
-        source: '/:path*', // Capture tout chemin après le domaine
-        destination: 'https://lavoiedubienetre.be/:path*', // Redirige vers la version sans `www`
-        permanent: true, // Indique une redirection 301, permanente
-        basePath: false, // S'assure que la redirection fonctionne même si basePath est défini
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
-
-
-
