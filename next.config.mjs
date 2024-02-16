780ce9b8fc7e9b726b41c8eff65a10ed0d1cdf87
stephane Gamot <116945810+White-Wolf-Web@users.noreply.github.com>
2024-02-16 14:00:46 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    async redirects() {
+      return [
+        {
+          source: '/((?!www).*)', // Tous les chemins sans "www"
+          destination: 'https://lavoiedubienetre.be/:path*', // Redirige vers la version sans "www", en conservant le chemin d'accès original
+          permanent: true, // Indique une redirection 301, permanente
+          has: [
+            {
+              type: 'host',
+              value: 'www.lavoiedubienetre.be',
+            },
+          ],
+        },
+      ];
+    },
+  };
+  
+  module.exports = nextConfig;
+  
